55638929d1072098daaba8ae23cf94a5b9eff8ee
Mikhail Bobrutskov <wizzardobox@gmail.com>
2024-01-17 21:52:47 +0100

    removed spaces in css

--- web-app/replace-react.js
+++ web-app/replace-react.js
@@ -21,13 +21,39 @@ const ls = (path, cb) => {
 
 fs.rmSync('buildts/node_modules/prop-types', { recursive: true, force: true });
 
+const removeSpacesInCSS = data => {
+    const regexPattern = /\b[a-z]+(\("[a-z]+"\))? `/g;
+    let regex = new RegExp(regexPattern);
+    let lastIndex = -1;
+    let match
+    let matches = []
+    while ((match = regex.exec(data)) !== null) {
+        let i = match.index;
+        i = data.indexOf('`', i) + 1
+        matches.push(i)
+
+        lastIndex = i;
+    }
+    matches.reverse().forEach(i => {
+        let end = data.indexOf('`', i)
+        let css = data.substring(i, end)
+        css = css.replaceAll(new RegExp(/\n\s+/g), '')
+        css = css.replaceAll(new RegExp(/: /g), ':')
+        css = css.replaceAll(new RegExp(/ \{/g), '{')
+        data = data.substring(0, i) + css + data.substring(end)
+        // console.log(data.substring(i, end))
+    })
+    return data;
+};
+
 ls('buildts', file => {
     if (!file.endsWith('.js'))
         return
 
     console.log(file)
 
-    let data = fs.readFileSync(file, {encoding: 'utf8', flag: 'r'});
+    let data = ''
+    data = fs.readFileSync(file, {encoding: 'utf8', flag: 'r'});
 
     data = data.replace(/from '(\.\.\/)*(node_modules\/)?react(-dom)?\/index.js'/, `from 'preact/compat'`)
     data = data.replace(/import '(\.\.\/)*(node_modules\/)?react(-dom)?\/index.js'/, `import 'preact/compat'`)
@@ -36,6 +62,8 @@ ls('buildts', file => {
     data = data.replace(/import propTypes from '(\.\.\/)*prop-types\/index.js'/, `import propTypes from 'prop-types'`)
     data = data.replace(/import PropTypes from '(\.\.\/)*prop-types\/index.js'/, `import PropTypes from 'prop-types'`)
     data = data.replace(/from '(\.\.\/)*(node_modules\/)?react\/jsx-runtime.js'/, `from 'preact/jsx-runtime'`)
+
+    data = removeSpacesInCSS(data);
 
 
     if (file === 'buildts/node_modules/react/cjs/react-jsx-runtime.development.js') {
